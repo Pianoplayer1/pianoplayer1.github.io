8eed53cb94535f1aa63f12d538d388c0408a9d74
Pianoplayer1 <alexander@capitain.me>
2022-11-08 21:33:58 +0100

    add clickable column headers for sorting

--- main.js
+++ main.js
@@ -16,8 +16,12 @@ document.getElementById('theme').addEventListener('change',e => {
 
 
 document.getElementById('treasury').addEventListener('input', treasury);
-document.getElementById('double').addEventListener('click', update);
-document.getElementById('storage').addEventListener('click', update);
+document.getElementById('double').addEventListener('click', () => update());
+document.getElementById('storage').addEventListener('click', () => update());
+
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', () => {
+    update(th);
+}))
 
 function treasury(e) {
     if ((e.target.value === '' ? '0' : e.target.value).match(/^\d{1,2}(?:[.,]\d{1,2})?$/)) {
@@ -28,12 +32,17 @@ function treasury(e) {
     }
 }
 
-function update() {
+function update(columnToSortBy) {
     const tbody = document.querySelector('.output tbody');
     while (tbody.rows.length) {
         tbody.deleteRow(0);
     }
-    let thead = document.querySelector('.output thead');
+    const thead = document.querySelector('.output thead');
+    if (columnToSortBy === undefined) {
+        Array.from(thead.querySelectorAll('th')).forEach(th => th.classList.remove('reversed'))
+        columnToSortBy = thead.querySelector('th:last-child');
+        columnToSortBy.classList.add('reversed')
+    }
     if (document.getElementById('storage').checked) {
         if (thead.querySelector('th').innerText !== 'Res. St.') {
             thead.querySelector('tr').insertCell(0).outerHTML = '<th>Res. St.</th>'
@@ -42,13 +51,12 @@ function update() {
         thead.querySelector('th').remove();
     }
 
+    const double = document.getElementById('double').checked
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j < 4; j++) {
             if (i === 0 && j === 0) continue;
             let treasury = parseFloat(document.getElementById('treasury').value);
-            let multiplier = (4 + 2 * i) / (4 - j)
-                * (1 + (isNaN(treasury) ? 0 : treasury) / 100)
-                * (document.getElementById('double').checked ? 2 : 1);
+            let multiplier = (4 + 2 * i) / (4 - j) * (1 + (isNaN(treasury) ? 0 : treasury) / 100) * (double ? 2 : 1);
 
             let cost = (i === 0 ? 0 : Math.pow(2, i) * 3000) + [0, 6000, 18000, 32000][j];
 
@@ -65,14 +73,19 @@ function update() {
             row.insertCell().innerText = j.toString();
             row.insertCell().innerText = cost.toString();
             row.insertCell().innerText = (multiplier * 3600).toFixed(0);
-            row.insertCell().innerText = ((multiplier - 1) * 360000 / cost).toFixed(2);
+            row.insertCell().innerText = (100 * (multiplier - (double ? 2 : 1)) * 3600 / cost).toFixed(2);
         }
     }
 
-    const th = thead.querySelector('th:last-child');
+    const columnIndex = Array.from(columnToSortBy.parentNode.children).indexOf(columnToSortBy)
     Array.from(tbody.querySelectorAll('tr:nth-child(n+1)'))
-        .sort(comparer(Array.from(th.parentNode.children).indexOf(th)))
+        .sort((row1, row2) => {
+            let value1 = row1.children[columnIndex].innerText || row1.children[columnIndex].textContent;
+            let value2 = row2.children[columnIndex].innerText || row2.children[columnIndex].textContent;
+            return columnToSortBy.classList.contains('reversed') ? value2 - value1 : value1 - value2;
+        })
         .forEach(tr => tbody.appendChild(tr));
+    columnToSortBy.classList.toggle('reversed')
 
     while (tbody.rows.length > 10) {
         tbody.deleteRow(10);
@@ -82,8 +95,5 @@ function update() {
         row.querySelector(':last-child').innerText += '%';
     }
 }
-
-const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-const comparer = (idx) => (a, b) => ((v1, v2) => v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2))(getCellValue(b, idx), getCellValue(a, idx));
 
 update();
